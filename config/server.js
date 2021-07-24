2c0afed67ae6f16b104d1ad269f68910eb03f018
avilla2 <a.villa12@outlook.com>
2021-07-24 18:39:32 +0000

    server updates

--- config/server.js
+++ config/server.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('NGINX_URL', 'https://api.betauo.com'),
+  url: env('NGINX_URL', 'admin.betauo.com'),
   admin: {
-    url: 'https://admin.betauo.com',
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
